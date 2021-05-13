2ccbf97a2ef49bdc9423d3936026b4a9f60203b0
Vitaliy <v.burla96@gmail.com>
2021-05-13 11:46:41 +0300

    prettier conf

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,20 +1,20 @@
-import React from "react";
-import Circle from "./Circle";
-import bugatti from "../img/bugatti_bg_fhd.jpeg";
+import React from 'react';
+import Circle from './Circle';
+import bugatti from '../img/bugatti_bg_fhd.jpeg';
 
 const Benefits = () => {
-  return (
-    <div className="benefits">
-      <div className="parallax">
-        {/* </div> */}
-        {/* </div> */}
-        <div className="circles">
-          <Circle title="Lorem" text="lorem ipsum lalla djksdsd" />
-          <Circle title="Lorem" text="lorem ipsum lalla djksttt" />
-          <Circle title="Lorem" text="lorem ipsum lalla djksss" />
+    return (
+        <div className='benefits'>
+            <div className='parallax'>
+                {/* </div> */}
+                {/* </div> */}
+                <div className='circles'>
+                    <Circle title='Lorem' text='lorem ipsum lalla djksdsd' />
+                    <Circle title='Lorem' text='lorem ipsum lalla djksttt' />
+                    <Circle title='Lorem' text='lorem ipsum lalla djksss' />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default Benefits;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,37 @@
-import React from "react";
-import SelectItem from "./SelectItem";
+import React from 'react';
+import SelectItem from './SelectItem';
 
 const Search = () => {
-  // let arr = [0, 1, 2, 3, 4];
-  return (
-    <div className="search-container">
-      <h2>USE QUICK SEARCH TO FIND YOUR CAR</h2>
-      <form className="search-form">
-        <div className="search-input">
-          <input type="text" name="" id="" placeholder="Your query" />
-          <button className="go-btn red-btn">Go</button>
-        </div>
-        <div className="select-container">
-          <h2>OR USE FILTERS</h2>
+    // let arr = [0, 1, 2, 3, 4];
+    return (
+        <div className='search-container'>
+            <h2>USE QUICK SEARCH TO FIND YOUR CAR</h2>
+            <form className='search-form'>
+                <div className='search-input'>
+                    <input type='text' name='' id='' placeholder='Your query' />
+                    <button className='go-btn red-btn'>Go</button>
+                </div>
+                <div className='select-container'>
+                    <h2>OR USE FILTERS</h2>
 
-          <div className="select-items">
-            <SelectItem title="Make" />
-            <SelectItem title="Model" />
-            <SelectItem title="Transmission" />
-            <SelectItem title="Body Color" />
-            <SelectItem title="Make" />
-            <SelectItem title="Model" />
-            <SelectItem title="Transmission" />
-            <SelectItem title="Body Color" />
-            <SelectItem title="Make" />
-            <SelectItem title="Model" />
-            <SelectItem title="Transmission" />
-            <SelectItem title="Body Color" />
-          </div>
+                    <div className='select-items'>
+                        <SelectItem title='Make' />
+                        <SelectItem title='Model' />
+                        <SelectItem title='Transmission' />
+                        <SelectItem title='Body Color' />
+                        <SelectItem title='Make' />
+                        <SelectItem title='Model' />
+                        <SelectItem title='Transmission' />
+                        <SelectItem title='Body Color' />
+                        <SelectItem title='Make' />
+                        <SelectItem title='Model' />
+                        <SelectItem title='Transmission' />
+                        <SelectItem title='Body Color' />
+                    </div>
+                </div>
+                <button className='find-btn red-btn'>Find</button>
+            </form>
         </div>
-        <button className="find-btn red-btn">Find</button>
-      </form>
-    </div>
-  );
+    );
 };
 export default Search;
